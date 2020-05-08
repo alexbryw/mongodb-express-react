77c57e1679a8127999a68e90589349d5b8f6118b
alexbryw <alexbryw@gmail.com>
2020-05-08 08:13:06 +0200

    Added login and password matching from db.
    TODO add cookie session.

--- server/routers/login.js
+++ server/routers/login.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const userRoute = require('../models/User.model')
 
 //Login.
-router.post('/api/user/login', function (req, res) {
-    res.json({msg:"from POST(login) /api/user/login"})
+router.post('/api/user/login', async function (req, res) {
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({msg:"Username or password is missing."})
+        return
+    }
+
+    const userFound = await userRoute.findOne({username: req.body.username})
+    if(userFound) {
+        const passwordMatch = await bcrypt.compare(req.body.password, userFound.password)
+        if(passwordMatch){
+            console.log("password match -V-")
+            console.log(passwordMatch)
+            res.json({msg:"Password is correct"})
+        } else {
+            res.status(400).json({msg:"Wrong password."})
+        }
+        // res.json({msg:"from POST(login) /api/user/login"})
+    } else {
+        res.status(400).json({msg:"User not found."})
+    }
+
 })
 
 //Logout.
