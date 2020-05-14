c333a873b1e76c7a9617138b533dab2f54b9b263
Oskar Lindblad <lindbladoskar@gmail.com>
2020-05-14 11:00:22 +0200

    welcome banner

---
+++ client/src/components/entry/WelcomeEntry.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Typography from '@material-ui/core/Typography';
+
+export default function EntryCard(props) {
+
+    let entryWidth = {   
+        width:'95vw',
+    }
+
+    const cardStyle = {
+        backgroundColor: '#fbc02d',
+        marginBottom: "1em"
+    }
+
+    if(useMediaQuery('(min-width:37em)')){
+        entryWidth = {   
+            width:'35em',
+        }
+    }
+
+    return (
+        <Grid item>
+            <Card style={{...cardStyle, ...entryWidth}}>
+                <Typography 
+                    variant="caption"
+                    component="h2"
+                    align="left"
+                    style={{margin:"1em"}}
+                    color="secondary"
+                >
+                    Välkommen {"BENGT"}
+                </Typography>
+            </Card>     
+        </Grid>
+  );
+}
